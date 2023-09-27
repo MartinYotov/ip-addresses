6cf9f714ba077914dc8edd50cee22d64b344e477
martin-econic <martin.yotov@eljoybikes.com>
2023-09-27 18:11:46 +0300

    Added default values for env variables

--- src/config/db.ts
+++ src/config/db.ts
@@ -2,10 +2,10 @@ import { Sequelize } from 'sequelize';
 import dotenv from 'dotenv'; 
 dotenv.config();
 
-const sequelize = new Sequelize(process.env.DB_NAME || 'postgres', process.env.DB_USER || 'postgres', process.env.DB_PASS, {
-    host: process.env.DB_HOST,
+const sequelize = new Sequelize(process.env.DB_NAME || 'postgres', process.env.DB_USER || 'postgres', process.env.DB_PASS || 'password', {
+    host: process.env.DB_HOST || 'localhost',
     dialect: 'postgres',
-    port: Number(process.env.DB_PORT),
+    port: Number(process.env.DB_PORT) || 5000,
 });
 
 const testDbConnection = async (): Promise<void> => {
